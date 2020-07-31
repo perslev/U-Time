86256a99b4bfabd15e9b6095b462be2f24ed82eb
Mathias Perslev <mathias@perslev.com>
2020-07-31 13:43:30 +0200

    Added support for ensemble predictions with channel groups

--- utime/bin/predict_one.py
+++ utime/bin/predict_one.py
@@ -11,6 +11,7 @@ from utime.bin.evaluate import (set_gpu_vis,
                                 get_logger)
 from utime import defaults
 from pprint import pformat
+from collections import namedtuple
 
 readline.parse_and_bind('tab: complete')
 
@@ -29,7 +30,12 @@ def get_argparser():
                         help="Output path for storing predictions.")
     parser.add_argument("--channels", nargs='*', type=str, default=None,
                         required=True,
-                        help="A list of channels to use for prediction.")
+                        help="A list of channels to use for prediction. "
+                             "To predict on multiple channel groups, pass one string for "
+                             "each channel group with each channel in the group separated by a '+' sign. "
+                             "E.g. to predict on EEG1-EOG1 and EEG2-EOG2, pass EEG1+EOG1 EEG2+EOG2. Each group "
+                             "will be used for prediction once, and the final results will be a majority vote "
+                             "across all.")
     parser.add_argument("--project_dir", type=str, default="./",
                         help='Path to U-Time project folder')
     parser.add_argument("--data_per_prediction", type=int, default=None,
@@ -54,10 +60,21 @@ def assert_args(args):
     return
 
 
-def predict_study(study, model, no_argmax):
-    # Predict
+def predict_study(study, model, channel_groups, no_argmax, logger=print):
     psg = np.expand_dims(study.get_all_periods(), 0)
-    pred = model.predict_on_batch(psg)
+    pred = None
+    for channel_group in channel_groups:
+        logger("--- Channel names: {}\n"
+               "    Channel inds:  {}".format(channel_group.channel_names,
+                                              channel_group.channel_indices))
+        # Get PSG for particular group
+        psg_subset = psg[..., tuple(channel_group.channel_indices)]
+        logger("    Extracted PSG shape: {}".format(psg_subset.shape))
+        if pred is None:
+            pred = model.predict_on_batch(psg_subset)
+        else:
+            # Sum into if using multiple channel groups
+            pred += model.predict_on_batch(psg_subset)
     pred = pred.numpy().reshape(-1, pred.shape[-1])
     if no_argmax:
         return pred
@@ -81,6 +98,36 @@ def save_prediction(pred, out_path, input_file_path, logger):
     np.save(out_path, pred)
 
 
+def unpack_channel_groups(channels):
+    """
+    TODO
+    """
+    channels_to_load, channel_groups = [], []
+    grouped = map(lambda chan: "+" in chan, channels)
+    if all(grouped):
+        for channel in channels:
+            group = channel.split("+")
+            channels_to_load.extend(group)
+            channel_groups.append(group)
+        # Remove duplicated while preserving order
+        from collections import OrderedDict
+        channels_to_load = list(OrderedDict.fromkeys(channels_to_load))
+    elif not any(grouped):
+        channels_to_load = channels
+        channel_groups = [channels]
+    else:
+        raise ValueError("Must specify either a list of channels "
+                         "or a list of channel groups, got a mix: {}".format(channels))
+    # Add channel inds to groups
+    channel_set = namedtuple("ChannelSet", ["channel_names", "channel_indices"])
+    for i, group in enumerate(channel_groups):
+        channel_groups[i] = channel_set(
+            channel_names=group,
+            channel_indices=[channels_to_load.index(channel) for channel in group]
+        )
+    return channels_to_load, channel_groups
+
+
 def get_sleep_study(psg_path,
                     logger,
                     **params):
@@ -101,7 +148,10 @@ def get_sleep_study(psg_path,
                        no_hypnogram=True,
                        period_length_sec=params.get('period_length_sec'),
                        logger=logger)
-    study.select_channels = params['channels']
+    channels_to_load, channel_groups = unpack_channel_groups(params['channels'])
+    logger("Loading channels: {}".format(channels_to_load))
+    logger("Channel groups: {}".format(channel_groups))
+    study.select_channels = channels_to_load
     study.sample_rate = params['set_sample_rate']
     study.scaler = params['scaler']
     study.set_quality_control_func(**params['quality_control_func'])
@@ -109,7 +159,7 @@ def get_sleep_study(psg_path,
     logger("Study loaded with shape: {}".format(study.get_psg_shape()))
     logger("Channels: {} (org names: {})".format(study.select_channels,
                                                  study.select_channels.original_names))
-    return study
+    return study, channel_groups
 
 
 def run(args):
@@ -134,9 +184,9 @@ def run(args):
     # Get the sleep study
     logger("Loading and pre-processing PSG file...")
     hparams['prediction_params']['channels'] = args.channels
-    study = get_sleep_study(psg_path=args.f,
-                            logger=logger,
-                            **hparams['prediction_params'])
+    study, channel_groups = get_sleep_study(psg_path=args.f,
+                                            logger=logger,
+                                            **hparams['prediction_params'])
 
     # Set GPU and get model
     set_gpu_vis(args.num_GPUs, args.force_GPU, logger)
@@ -149,9 +199,9 @@ def run(args):
         logger=logger,
         weights_file_name=hparams.get_from_anywhere('weight_file_name')
     )
-
     logger("Predicting...")
-    pred = predict_study(study, model, args.no_argmax)
+    pred = predict_study(study, model, channel_groups, args.no_argmax, logger)
+    logger("--> Predicted shape: {}".format(pred.shape))
     save_prediction(pred=pred,
                     out_path=args.o,
                     input_file_path=study.psg_file_path,
