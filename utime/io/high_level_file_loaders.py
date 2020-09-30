785983aa3df36d033102e1f6fe9c83d4c5f6b7c8
Mathias Perslev <mathias@perslev.com>
2020-09-30 17:37:52 +0200

    Added duplicate check at a higher level for included channels

--- utime/io/high_level_file_loaders.py
+++ utime/io/high_level_file_loaders.py
@@ -16,7 +16,8 @@ from utime.errors import ChannelNotFoundError
 
 def get_org_include_exclude_channel_montages(load_channels, header,
                                              ignore_reference_channels=False,
-                                             check_num_channels=True):
+                                             check_num_channels=True,
+                                             check_duplicates=True):
     """
     TODO
 
@@ -50,6 +51,15 @@ def get_org_include_exclude_channel_montages(load_channels, header,
             )
     else:
         include_channels = channels_in_file
+    if check_duplicates:
+        for channel in include_channels:
+            if channels_in_file.count(channel) > 1:
+                raise ValueError(f"Cannot load channel with name \"{channel.original_name}\" as this channel "
+                                 f"name occurs multiple times in the file: {channels_in_file.original_names}). "
+                                 f"This is most likely a result of longer channel names which were truncated to "
+                                 f"identical, shorter names due to limitations in the used file format. "
+                                 f"E.g. EDF files may only store channel names at most 16 characters in length. "
+                                 f"Please rename your channels and try again.")
     exclude_channels = [c for c in channels_in_file if c not in include_channels]
     exclude_channels = ChannelMontageTuple(exclude_channels)
     return channels_in_file, include_channels, exclude_channels
@@ -104,7 +114,8 @@ def load_psg(psg_file_path,
             load_channels=load_channels,
             header=header,
             ignore_reference_channels=ignore_reference_channels,
-            check_num_channels=check_num_channels
+            check_num_channels=check_num_channels,
+            check_duplicates=True
         )
     header["channel_names"] = include_channels
     header["n_channels"] = len(include_channels)
