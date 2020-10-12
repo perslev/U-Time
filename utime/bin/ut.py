d7305ab74672bb16f192e1f96e0717de316ab908
Mathias Perslev <mathias@perslev.com>
2020-10-12 10:30:46 +0200

    Added ability to seed any ut script invoked by the ut.py entry script

--- utime/bin/ut.py
+++ utime/bin/ut.py
@@ -3,11 +3,13 @@ Entry script redirecting all command line arguments to a specified script
 from the utime.bin folder.
 
 Usage:
-ut [script] [script args...]
+ut [--help] script [script args]... [--seed]
 """
 
 import argparse
 import os
+import sys
+import utime
 
 
 def get_parser():
@@ -17,7 +19,7 @@ def get_parser():
 
     ids = "U-Time ({})".format(__version__)
     sep = "-" * len(ids)
-    usage = ("utime [script] [script args...]\n\n"
+    usage = ("ut [--help] script [script args]... [--seed]\n\n"
              "%s\n%s\n"
              "Available scripts:\n") % (ids, sep)
 
@@ -37,22 +39,36 @@ def get_parser():
     parser = argparse.ArgumentParser(usage=usage)
     parser.add_argument("script", help="Name of the mp script to run.",
                         choices=choices)
-    parser.add_argument("args", help="Arguments passed to script",
-                        nargs=argparse.REMAINDER)
+    parser.add_argument("--seed", default=None, type=int,
+                        help="Run this script with numpy, random and tensorflow RNGs seeded "
+                             "from integer --seed.")
     return parser
+
+
+def split_help_from_args(args):
+    other_args, help_args = [], []
+    for arg in args:
+        if arg == "-h" or arg == "--help":
+            help_args.append("--help")
+        else:
+            other_args.append(arg)
+    return other_args, help_args
 
 
 def entry_func():
     # Get the script to execute, parse only first input
-    parsed = get_parser().parse_args()
+    args, help_agrs = split_help_from_args(sys.argv[1:])
+    parsed, script_args = get_parser().parse_known_args(args or help_agrs)
     script = parsed.script
+    if parsed.seed is not None:
+        utime.Defaults.set_global_seed(parsed.seed)
 
     # Import the script
     import importlib
     mod = importlib.import_module("utime.bin." + script)
 
     # Call entry function with remaining arguments
-    mod.entry_func(parsed.args)
+    mod.entry_func(script_args + help_agrs)
 
 
 if __name__ == "__main__":
