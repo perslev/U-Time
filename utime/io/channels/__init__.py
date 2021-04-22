7d25598f509c21ce9c0817e67d80a2c2c3deeb3e
Mathias Perslev <mathias@perslev.com>
2021-04-22 13:56:43 +0200

    Moved channel_types.py to io.channels; infer_channel_types now also attempts to classify MASTOID channels

--- utime/io/channels/__init__.py
+++ utime/io/channels/__init__.py
@@ -2,3 +2,4 @@ from .channels import ChannelMontage, ChannelMontageTuple
 from .montage_creator import ChannelMontageCreator
 from .random_channel_selector import (RandomChannelSelector,
                                       filter_non_available_channels)
+from .channel_types import infer_channel_types
--- utime/io/header/__init__.py
+++ utime/io/header/__init__.py
@@ -1,2 +1,1 @@
 from .header_extractors import extract_header
-from .channel_types import infer_channel_types
