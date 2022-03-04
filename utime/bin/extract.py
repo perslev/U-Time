fd311706b6ddc8e57e9f71ea1e72cccbca53c68e
Mathias Perslev <mathias@perslev.com>
2022-03-04 11:40:28 +0100

    Now imports from sleeputils

--- utime/bin/extract.py
+++ utime/bin/extract.py
@@ -7,12 +7,17 @@ The PSG file must be loadable using:
 utime.io.high_level_file_loaders import load_psg
 """
 
+import os
+import numpy as np
+import pickle
 from argparse import ArgumentParser
 from glob import glob
-import os
-from utime.errors import ChannelNotFoundError
-from utime.io.channels import ChannelMontageTuple, ChannelMontageCreator
-from utime.io.header import extract_header
+from sleeputils.errors import ChannelNotFoundError
+from sleeputils.io.channels import ChannelMontageTuple, ChannelMontageCreator
+from sleeputils.io.header import extract_header
+from sleeputils.io.high_level_file_loaders import load_psg
+from sleeputils.io import to_h5_file
+from sleeputils.preprocessing.psg_sampling import set_psg_sample_rate
 from mpunet.logging import Logger
 
 
@@ -52,6 +57,11 @@ def get_argparser():
     parser.add_argument("--use_dir_names", action="store_true",
                         help='Each PSG file will be saved as '
                              '<parent directory>.h5 instead of <file_name>.h5')
+    parser.add_argument("--trim_leading_seconds_dict", type=str, default=None,
+                        help="Path to .pickle dictionary of format ['filename' (without extension): float 'seconds'] "
+                             "where 'seconds' is the number of seconds to trim from the start of file 'filename' before "
+                             "saving to newly extracted file. Note that 'filename' is the dictionary name if the "
+                             "--use_dir_names flag is also set.")
     return parser
 
 
@@ -67,12 +77,10 @@ def _extract(file_,
              out_path,
              channels,
              renamed_channels,
+             trim_leading_sec,
              logger,
              args):
-    from utime.io.high_level_file_loaders import load_psg
-    from utime.utils.scriptutils import to_h5_file
     channels_in_file = extract_header(file_)["channel_names"]
-
     chan_creator = ChannelMontageCreator(existing_channels=channels_in_file,
                                          channels_required=channels,
                                          allow_missing=True)
@@ -92,18 +100,34 @@ def _extract(file_,
     # create montages
     psg, channels = chan_creator.create_montages(psg)
     header['channel_names'] = channels
+    logger("[*] Original PSG shape: {}".format(psg.shape))
+
+    # Trim if needed
+    if trim_leading_sec and not np.isnan(trim_leading_sec):
+        n_trim = trim_leading_sec * header['sample_rate']
+        if not np.isclose(n_trim, int(n_trim)):
+            logger.warn(f"The number of trim seconds {trim_leading_sec} does not give a "
+                        f"whole number of datapoints to remove ({n_trim}) given sample rate "
+                        f"{header['sample_rate']}. Skipping file!")
+            return
+        n_trim = int(n_trim)
+        psg = psg[n_trim:]
+        logger(f"[*] PSG shape after trimming (leading, seconds={trim_leading_sec}, sr={header['sample_rate']}, "
+               f"N trim={n_trim}): {psg.shape}")
+
+    if psg.shape[0] % header['sample_rate']:
+        logger(f"--- OBS: Length {len(psg)} not divisible by sample rate! "
+               f"Trimming N items from end {len(psg) % header['sample_rate']}.")
+        psg = psg[:-(psg.shape[0] % header['sample_rate'])]
+        logger("--- New PSG shape: {}".format(psg.shape))
 
     # Resample
-    logger("[*] PSG shape before re-sampling: {}".format(psg.shape))
     if args.resample:
-        from utime.preprocessing.psg_sampling import set_psg_sample_rate
         psg = set_psg_sample_rate(psg,
                                   new_sample_rate=args.resample,
                                   old_sample_rate=header['sample_rate'])
         header['sample_rate'] = args.resample
-        if psg.shape[0] % args.resample:
-            logger("ERROR: Not divisible by sample rate!")
-    logger("[*] PSG shape after re-sampling: {}".format(psg.shape))
+        logger("[*] PSG shape after re-sampling: {}".format(psg.shape))
 
     # Rename channels
     if renamed_channels:
@@ -118,7 +142,7 @@ def _extract(file_,
     to_h5_file(out_path, psg, **header)
 
 
-def extract(files, out_dir, channels, renamed_channels, logger, args):
+def extract(files, out_dir, channels, renamed_channels, trim_leading_seconds_dict, logger, args):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     for i, file_ in enumerate(files):
@@ -140,14 +164,35 @@ def extract(files, out_dir, channels, renamed_channels, logger, args):
                 raise OSError(f"File already exists at '{out_path}' abd neither --overwrite nor --continue was set.")
             else:
                 os.remove(out_path)
+        trim_leading_secs = 0.0
+        if trim_leading_seconds_dict:
+            if name not in trim_leading_seconds_dict:
+                raise ValueError(f"The filename '{name}' does not exist as a key in dictioanry "
+                                 f"'trim_leading_seconds_dict'. All files to extract must be represented in the trim "
+                                 f"dictionary. If you do not want to trim this particular file, enter the filename "
+                                 f"anyway into the dictionary with key:value pair ({name}: 0.0) to effectively apply "
+                                 f"no trimming.")
+            trim_leading_secs = trim_leading_seconds_dict[name]
         _extract(
             file_=file_,
             out_path=out_path,
             channels=channels,
             renamed_channels=renamed_channels,
+            trim_leading_sec=trim_leading_secs,
             logger=logger,
             args=args
         )
+
+
+def get_trim_dict(path):
+    if not os.path.exists(path):
+        raise OSError(f"No trim dict exists at path {path}")
+    with open(path, "rb") as in_f:
+        trim_dict = pickle.load(in_f)
+    if not isinstance(trim_dict, dict):
+        raise ValueError(f"The trim dict at path {path} with has an unexpected type ({type(trim_dict)}) "
+                         f"- expected {dict}")
+    return trim_dict
 
 
 def run(args):
@@ -179,12 +224,17 @@ def run(args):
         logger("Saving channels under names {}".format(renamed_channels))
     logger("Saving .h5 files to '{}'".format(out_dir))
     logger("Re-sampling: {}".format(args.resample))
+    trim_leading_seconds_dict = None
+    if args.trim_leading_seconds_dict:
+        logger(f"Using (trailing) trim dict at path '{args.trim_leading_seconds_dict}'")
+        trim_leading_seconds_dict = get_trim_dict(args.trim_leading_seconds_dict)
     logger("-*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*-")
     extract(
         files=files,
         out_dir=out_dir,
         channels=channels,
         renamed_channels=renamed_channels,
+        trim_leading_seconds_dict=trim_leading_seconds_dict,
         logger=logger,
         args=args
     )
