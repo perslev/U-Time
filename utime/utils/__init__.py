2d6b31f6cda18b83a1febb47a7c2b32dbb7c54f0
Mathias Perslev <mathias@perslev.com>
2022-04-06 13:47:21 +0200

    Added utility functions from mpunet

--- utime/utils/__init__.py
+++ utime/utils/__init__.py
@@ -1,1 +1,1 @@
-from .utils import create_folders, highlighted, await_pids, set_gpu
+from .utils import create_folders, highlighted, await_pids
---
+++ utime/utils/utils.py
@@ -0,0 +1,71 @@
+import logging
+import os
+import subprocess
+import time
+
+logger = logging.getLogger(__name__)
+
+
+def create_folders(folders, create_deep=False):
+    def safe_make(path, make_func):
+        try:
+            make_func(path)
+        except FileExistsError:
+            # If running many jobs in parallel this may occur
+            pass
+    make_func = os.mkdir if not create_deep else os.makedirs
+    if isinstance(folders, str):
+        if not os.path.exists(folders):
+            safe_make(folders, make_func)
+    else:
+        folders = list(folders)
+        for f in folders:
+            if f is None:
+                continue
+            if not os.path.exists(f):
+                safe_make(f, make_func)
+
+
+def highlighted(string):
+    length = len(string) if "\n" not in string else max([len(s) for s in string.split("\n")])
+    border = "-" * length
+    return "%s\n%s\n%s" % (border, string, border)
+
+
+def await_pids(pids, check_every=120):
+    if isinstance(pids, str):
+        for pid in pids.split(","):
+            wait_for(int(pid), check_every=check_every)
+    else:
+        wait_for(pids, check_every=check_every)
+
+
+def wait_for(pid, check_every=120):
+    """
+    Check for a running process with pid 'pid' and only return when the process
+    is no longer running. Checks the process list every 'check_every' seconds.
+    """
+    if not pid:
+        return
+    if not isinstance(pid, int):
+        try:
+            pid = int(pid)
+        except ValueError as e:
+            raise ValueError(f"Cannot wait for pid '{pid}', must be an integer") from e
+    _wait_for(pid, check_every)
+
+
+def _wait_for(pid, check_every=120):
+    still_running = True
+    logging.info(f"\n[*] Waiting for process pid={pid} to terminate...")
+    while still_running:
+        ps = subprocess.Popen(("ps", "-p", f"{pid}"), stdout=subprocess.PIPE)
+        try:
+            output = subprocess.check_output(("grep", f"{pid}"), stdin=ps.stdout)
+        except subprocess.CalledProcessError:
+            output = False
+        ps.wait()
+        still_running = bool(output)
+        if still_running:
+            logging.info(f"Process {pid} still running... (sleeping {check_every} seconds)")
+            time.sleep(check_every)
