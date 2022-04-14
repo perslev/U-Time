9fc38cdca0fd1d96ed5088977bc16cb13cb14f9c
Mathias Perslev <mathias@perslev.com>
2022-04-14 14:56:02 +0200

    Added checks and handlers for depcreated hyperparameter files

--- utime/hyperparameters/__init__.py
+++ utime/hyperparameters/__init__.py
@@ -1,9 +1,78 @@
+import logging
 from utime import Defaults
+from psg_utils.utils import ensure_list_or_tuple
 from yamlhparams import YAMLHParams as _YAMLHParams
+
+logger = logging.getLogger(__name__)
+
+
+def _handle_channel_sampling_group_renaming(hparams):
+    """
+    Replace 'access_time_channel_sampling_groups' and/or 'load_time_channel_sampling_groups'
+    attributes with the new common attribute 'channel_sampling_groups'.
+    Required as per utime v1.0.0 due to API changes in psg_utils package.
+    """
+    new_name = "channel_sampling_groups"
+    for deprecated_name in ('access_time_channel_sampling_groups', 'load_time_channel_sampling_groups'):
+        value = hparams.get(deprecated_name, None)
+    if value is not None:
+        logger.warning(f"Found deprecated hyperparameter value '{deprecated_name}' in hyperparameter file "
+                       f"at path {hparams.yaml_path}. Renaming to '{new_name}' and saving "
+                       f"hyperparameters to disk.")
+        hparams.delete_group(deprecated_name)
+        hparams.set_group(f"/{new_name}", value=value)
+        hparams.save_current()
+
+
+def _handle_metrics_renaming(hparams):
+    """
+    Replace metrics specified in the old TF function 'sparse_metric_name' format with
+    new class 'SparseMetricName' format. Required as per utime v1.0.0
+    """
+    map_ = {
+        "sparse_categorical_accuracy": "SparseCategoricalAccuracy",
+        "sparse_categorical_crossentropy": "SparseCategoricalCrossentropy",
+        "sparse_top_k_categorical_accuracy": "SparseTopKCategoricalAccuracy"
+    }
+    try:
+        metrics = ensure_list_or_tuple(hparams.get_group('/fit/metrics'))
+    except KeyError:
+        pass
+    else:
+        any_replaced = False
+        for i, metric in enumerate(metrics):
+            if metric in map_:
+                logger.warning(f"Found deprecated metrics naming of '{metric}' in hyperparameter file "
+                               f"at path {hparams.yaml_path}. Renaming to '{map_[metric]}' and "
+                               f"saving hyperparameters to disk.")
+                any_replaced = True
+                metrics[i] = map_[metric]
+        if any_replaced:
+            hparams.set_group("/fit/metrics", value=metrics, overwrite=True)
+            hparams.save_current()
+
+
+def _handle_version_format_changes(hparams):
+    """
+    Check if hparams file has old format __VERSION__, __COMMIT__ and/or __BRANCH__ tags.
+    If so, warn the user and suggest them to remove them manually (we do not delete as some users
+    may need to manually refer to these versions as no version control will be made against them anymore).
+    """
+    tags_to_check = ('__VERSION__', '__COMMIT__', '__BRANCH__')
+    for tag in tags_to_check:
+        if hparams.get(tag):
+            logger.warning(f"Found deprecated version controlling attribute naming of '{tag}' in "
+                           f"hyperparameter file at path {hparams.yaml_path}. "
+                           f"Since utime v1.0.0 this and other version control related attributes have been "
+                           f"renamed. The new attributes will be automatically added to this file and the old tags "
+                           f"will remain. However, no version controlling will be made against the OLD tags. "
+                           f"To suppress this warning, delete the old tag manually from the hyperparameters file.")
 
 
 def check_deprecated_params(hparams):
-    pass
+    _handle_channel_sampling_group_renaming(hparams)
+    _handle_metrics_renaming(hparams)
+    _handle_version_format_changes(hparams)
 
 
 class YAMLHParams(_YAMLHParams):
